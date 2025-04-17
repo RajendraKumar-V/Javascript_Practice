669cbe1d717f414befb4743842c9922bfcdd4b9e
RajendraKumarV <rajendravaradaraju@example.com>
2025-04-17 14:50:37 +0530

    implemented primitive and objects

---
+++ object.js
@@ -0,0 +1,13 @@
+const items={
+    "Raj":false,
+    "Madhu":true,
+    "Mahendra":45,
+    "Raaghu":null,
+    "Ravi":undefined,
+}
+
+console.log(items["Raj"])
+console.log(items["Madhu"])
+console.log(items["Mahendra"])
+console.log(items["Raaghu"])
+console.log(items["Ravi"])
---
+++ primitives.js
@@ -0,0 +1,7 @@
+let a = BigInt(1234567890123456789012345678901234567890);
+console.log(a)
+console.log(typeof a)
+
+let b =Symbol('Hello');
+console.log(b)
+//nn bb ss u
