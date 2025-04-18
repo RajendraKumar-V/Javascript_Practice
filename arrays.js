faadb32314c8437b31b1283980f2a3f8a5d959b3
RajendraKumarV <rajendravaradaraju@example.com>
2025-04-18 19:46:18 +0530

    implemented loops and arrays

---
+++ arrays.js
@@ -0,0 +1,28 @@
+/*let valArray = [1,2,3,2,4];
+console.log(valArray);
+console.log(valArray.length)
+
+console.log(Array.isArray(valArray));
+
+const valObj = {like:'850crores'};
+console.log(Array.isArray(valObj))*/
+
+//Push adding elements to the end of an array and returns the new length of the array
+/*let arrVal = [1,2,3];
+console.log(arrVal.push(4,5)); //4
+console.log(arrVal)
+
+//pop removing the last element of an array and returns that element
+console.log(arrVal.pop()); //4
+*/
+
+//shift 
+//let valArrayCheck = [1,2,3,4,5];
+//console.log(valArrayCheck.shift())//removes the first element of an array and returns that element
+//unshift 
+//console.log(valArrayCheck.unshift())
+
+//splice
+//let spliceArray=[3,4,5,6,7,8];
+//console.log(spliceArray.splice(2,3))
+//console.log(spliceArray);
---
+++ loops.js
@@ -0,0 +1,25 @@
+//for loops
+/*let sum = 0;
+let n=3
+for(let i=0;i<n;i++){
+    //sum= sum+i
+    sum+=i+1; // sum = sum + i + 1
+}
+console.log("Final sum is: " + sum); //o/p 6*/
+
+//for in loop
+/*let obj={
+    Rajendra:61,
+    madhu:80,
+    Samu:100
+}
+for(let key in obj){
+    console.log(key +":"+obj[key])
+}*/
+
+//while loop
+/*let i=0;
+while(i<5){
+    console.log("Hello World " + i);
+    i++;
+}*/
