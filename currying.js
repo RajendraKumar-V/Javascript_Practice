9dc60d3ebfa22b98e116ce8bb099952f8da4c424
RajendraKumarV <rajendravaradaraju@example.com>
2025-04-21 21:29:27 +0530

    Learnt currying

---
+++ currying.js
@@ -0,0 +1,11 @@
+// let sum = function(a){
+//     return function(b){
+//         return function (c){
+//             return a+b+c;
+//         }
+//     }
+// }
+// const sum =a=>b=>c=>a+b+c
+const sum =a=>b=>c=>a+b+C
+
+console.sum(1)(2)(3); //6
