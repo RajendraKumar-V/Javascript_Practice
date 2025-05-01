e1ff4ef19b015b32b5515d8e4c28fe0eb501aea5
RajendraKumarV <rajendravaradaraju@example.com>
2025-05-01 17:23:44 +0530

    learnt promise all,allSetteled,rrace,any

--- promise.js
+++ promise.js
@@ -1,14 +1,64 @@
+// let pizzaPromise = new Promise((resolve,reject)=>{
+//     let pizzaReady = true
+//     if(pizzaReady){
+//         resolve('Pizza is ready')
+//     }else{
+//         reject('Pizza is not ready')
+//     }
+// })
+// pizzaPromise.then((message)=>{
+//     console.log(message)
+// })
+// .catch((error)=>{
+//     console.error(error)
+// })
 let pizzaPromise = new Promise((resolve,reject)=>{
-    let pizzaReady = true
-    if(pizzaReady){
-        resolve('Pizza is ready')
+    let pizza = true
+    if(pizza){
+      resolve('Pizza is ready')
     }else{
-        reject('Pizza is not ready')
+      reject('pizza is not ready')
     }
 })
-pizzaPromise.then((message)=>{
-    console.log(message)
+
+
+let churmuriPromise = new Promise((resolve,reject)=>{
+  let churmuri = true
+  if(churmuri){
+    resolve('churmuri is ready')
+  }else{
+    reject('churmuri is not ready')
+  }
 })
-.catch((error)=>{
-    console.error(error)
+
+
+
+Promise.allSettled([pizzaPromise,churmuriPromise])
+.then((items) => {
+console.log("All delivered:", items);
 })
+.catch((error) => {
+console.log("One failed:", error);
+});
+
+Promise.all([pizzaPromise,churmuriPromise])
+.then((items) => {
+console.log("All delivered:", items);
+})
+.catch((error) => {
+console.log("One failed:", error);
+});
+Promise.race([pizzaPromise,churmuriPromise])
+.then((items) => {
+console.log("All delivered:", items);
+})
+.catch((error) => {
+console.log("One failed:", error);
+});
+Promise.any([pizzaPromise,churmuriPromise])
+.then((items) => {
+console.log("All delivered:", items);
+})
+.catch((error) => {
+console.log("One failed:", error);
+});
